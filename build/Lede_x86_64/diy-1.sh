e9ec2db144597afe05a6e88b5124fda44c174eb3
R6400 <38423539+R6400@users.noreply.github.com>
2021-01-23 12:49:23 +0800

    Update diy-1.sh

--- build/Lede_x86_64/diy-1.sh
+++ build/Lede_x86_64/diy-1.sh
@@ -7,3 +7,17 @@
 # 相同的文件都拉一起，因为有一些可能还是其他大神修改过的容易造成编译错误的
 # 想要什么插件就单独的拉取什么插件就好，或者告诉我，我把插件放我的插件包就行了
 # 软件包地址：https://github.com/281677160/openwrt-package
+#网络拓扑https://github.com/DavBfr/luci-app-netmap    
+git clone https://github.com/DavBfr/luci-app-netmap package/luci-app-netmap
+#在线用户
+git clone https://github.com/rufengsuixing/luci-app-onliner package/luci-app-onliner
+#SoftEther VPN https://github.com/ZhanhuaJin/openwrt-package/tree/bae9c5ff772221c4003ad8223ab86144be2a8dad/lienol/luci-app-softethervpn
+svn co https://github.com/ZhanhuaJin/openwrt-package/trunk/lienol/luci-app-softethervpn package/luci-app-softethervpn
+#PingOS
+svn co https://github.com/ZhanhuaJin/openwrt-package/trunk/lienol/luci-app-nginx-pingos package/luci-app-nginx-pingos
+#BaiduPCS-Web
+git clone https://github.com/KFERMercer/luci-app-baidupcs-web package/luci-app-baidupcs-web
+#PwdHackDeny     异常登录拒绝
+svn co https://github.com/hong0980/build/trunk/luci-app-PwdHackDeny package/luci-app-PwdHackDeny
+#luci-app-netdata      实时监控   
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
